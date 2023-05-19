013075b8dc1cbbd94e6eade962cef338be3f865c
YishiChen <yishdk@gmail.com>
2023-05-19 22:50:44 +0200

    high complex 10 ccn + relu

--- detr-main/run_with_submitit.py
+++ detr-main/run_with_submitit.py
@@ -21,8 +21,6 @@ def parse_args():
     parser.add_argument("--nodes", default=1, type=int, help="Number of nodes to request")
     parser.add_argument("--timeout", default=60 * 24 * 4, type=int, help="Duration of the job")
     parser.add_argument("--job_dir", default="", type=str, help="Job dir. Leave empty for automatic.")
-
-    # parser.add_argument("--resume", default="/scratch/s203877/checkpoints/3365/checkpoint0019.pth", type=str, help="resume path.")
     return parser.parse_args()
 
 
@@ -105,10 +103,10 @@ def main():
         cpus_per_task=4,
         nodes=nodes,
         timeout_min=timeout_min,  # max is 60 * 72
-        slurm_additional_parameters={"nodelist": "comp-gpu06"}
+        slurm_additional_parameters={"nodelist": "comp-gpu07"}
     )
 
-    executor.update_parameters(name="low-bb")
+    executor.update_parameters(name="detr-high")
 
     args.dist_url = get_init_file().as_uri()
     args.output_dir = args.job_dir
