c4f4decab9deb1239e0d49f91ff62f7702998446
YishiChen <yishdk@gmail.com>
2023-05-23 00:11:43 +0200

    lr 25

--- detr-main/main.py
+++ detr-main/main.py
@@ -32,7 +32,7 @@ def get_args_parser():
     parser.add_argument('--batch_size', default=5, type=int)
     parser.add_argument('--weight_decay', default=1e-4, type=float)
     parser.add_argument('--epochs', default=300, type=int)
-    parser.add_argument('--lr_drop', default=100, type=int)
+    parser.add_argument('--lr_drop', default=25, type=int)
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
                         help='gradient clipping max norm')
 
