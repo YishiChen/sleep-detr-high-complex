7ab2365b902c7b51c1018ae6053e9682f6a77516
YishiChen <85686404+YishiChen@users.noreply.github.com>
2023-06-08 23:19:22 +0200

    Update main.py
    eos = 0.05

--- detr-main/main.py
+++ detr-main/main.py
@@ -83,7 +83,7 @@ def get_args_parser():
     parser.add_argument('--dice_loss_coef', default=0, type=float)
     parser.add_argument('--bbox_loss_coef', default=5, type=float)
     parser.add_argument('--giou_loss_coef', default=2, type=float)
-    parser.add_argument('--eos_coef', default=0.1, type=float,
+    parser.add_argument('--eos_coef', default=0.05, type=float,
                         help="Relative classification weight of the no-object class")
 
     # dataset parameters
